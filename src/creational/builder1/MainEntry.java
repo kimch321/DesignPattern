9dea3b6686c28d29cfbf0d36f739ee0ededd6f80
KIM CHUNG IL <cnddlf12@naver.com>
2023-07-02 19:33:41 +0900

    Builder + Strategy

--- src/creational/builder1/MainEntry.java
+++ src/creational/builder1/MainEntry.java
@@ -2,14 +2,13 @@ package creational.builder1;
 
 import java.util.Random;
 
-public class MainEntity {
+public class MainEntry {
 
 	public static void main(String[] args) {	
 		Car car1 = new Car("V7", true, "Black", true, false);
 		
 		// 생성자를 이용해 호출하는 방식과 달리 builder패턴으로 객체를 생성하면 객체의 스펙 하나하나를 모두 지정할 수 있습니다.
 		// 뿐만 아니라 객체의 생성을 원하는 만큼 지연시킬 수 있습니다.
-		// 객체의 생성 과정과 표현 방법을 분리하고 있어서, 동일한 객체 생성에서도 서로 다른 결과를 만들 수 있다.
 		Car car2 = new CarBuilder()
 				.setAEB(false)
 				.setAirbag(false)
---
+++ src/creational/builder2/Data.java
@@ -0,0 +1,20 @@
+package creational.builder2;
+
+public class Data {
+	private String name;
+	private int age;
+	
+	public Data(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+}
